c880867a36e03ed270486bcd0bc4b0708fc59a06
Neamaddin <neamaddin.a@mail.ru>
2020-02-16 01:39:57 +0600

    v1.2

--- src/config/settings.py
+++ src/config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Portal.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Portal.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
